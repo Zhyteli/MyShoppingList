0f1f389afc79700e5bc93b91f3c9cf44a5f83532
Asus <kirill.zhikhor15@gmail.com>
2022-07-26 17:04:23 +0300

    Initial commit

---
+++ app/src/main/java/com/zhytel/myshoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zhytel.myshoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zhytel.myshoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
