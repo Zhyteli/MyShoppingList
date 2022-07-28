61b08f35797235aeeb127b952c5c5161b3ddb4a0
Asus <kirill.zhikhor15@gmail.com>
2022-07-28 17:24:37 +0300

    Улучшеный способ обработки RecyclerView

--- app/src/main/java/com/zhytel/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/zhytel/myshoppinglist/presentation/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.zhytel.myshoppinglist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.zhytel.myshoppinglist.R
-import com.zhytel.myshoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
-            shopListAdapter.shopList = it
+            shopListAdapter.submitList(it)
         }
         viewModel
     }
@@ -52,7 +51,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val item = shopListAdapter.shopList[viewHolder.adapterPosition]
+                val item = shopListAdapter.currentList[viewHolder.adapterPosition]
                 viewModel.deleteShopItem(item)
             }
         }
--- app/src/main/java/com/zhytel/myshoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/zhytel/myshoppinglist/presentation/ShopListAdapter.kt
@@ -1,51 +1,32 @@
 package com.zhytel.myshoppinglist.presentation
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.zhytel.myshoppinglist.R
 import com.zhytel.myshoppinglist.domain.ShopItem
-import java.lang.RuntimeException
 
-class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
 
-    var shopList = listOf<ShopItem>()
-        set(value){
-            val callback = ShopListDiffCallback(shopList, value)
-            val diffCallback = DiffUtil.calculateDiff(callback)
-            diffCallback.dispatchUpdatesTo(this)
-            field = value
-        }
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
-    var count:Int = 0
 
-    class ShopItemViewHolder(val view: View):RecyclerView.ViewHolder(view) {
-        val tvName: TextView = view.findViewById(R.id.tv_name)
-        val tvCount: TextView = view.findViewById(R.id.tv_count)
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val layout = when(viewType){
+        val layout = when (viewType) {
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
             VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
             else -> throw RuntimeException("Не тот тип $viewType")
         }
         val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
-        ++count
-        Log.d("count", count.toString())
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val shopItem = shopList[position]
+        val shopItem = getItem(position)
         holder.tvName.text = shopItem.name
         holder.tvCount.text = shopItem.count.toString()
-        holder.view.setOnClickListener{
+        holder.view.setOnClickListener {
             onShopItemLongClickListener?.invoke(shopItem)
             onShopItemClickListener?.invoke(shopItem)
             true
@@ -53,18 +34,15 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
     }
 
     override fun getItemViewType(position: Int): Int {
-        val item = shopList[position]
-        return if (item.enabled){
+        val item = getItem(position)
+        return if (item.enabled) {
             VIEW_TYPE_ENABLED
-        }else{
+        } else {
             VIEW_TYPE_DISABLED
         }
     }
 
-    override fun getItemCount(): Int {
-        return shopList.size
-    }
-    companion object{
+    companion object {
         const val VIEW_TYPE_ENABLED = 0
         const val VIEW_TYPE_DISABLED = 1
 
